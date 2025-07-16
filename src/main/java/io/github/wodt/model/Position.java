b9fcc4c8d7d8afaa55234c10904d7d39751cbff2
Matteo Sartini <matteo.sartini26@gmail.com>
2025-07-16 11:42:11 +0200

    Added Position json parser

--- src/main/java/io/github/wodt/model/Position.java
+++ src/main/java/io/github/wodt/model/Position.java
@@ -1,5 +1,10 @@
 package io.github.wodt.model;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.function.Function;
+
 /** A set of coordinates inside the world */
 public class Position {
     private final double latitude;
@@ -29,5 +34,34 @@ public class Position {
      */
     public double getLongitude() {
         return this.longitude;
+    }
+
+    /**
+     * Returns the Position from a given jsonString.
+     * @param jsonString Json string.
+     * @return The position, if found.
+     */
+    public static Position extractFromJSON(String jsonString) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(jsonString);
+            JsonNode coordinatesNode = root.get("coords");
+            double x = coordinatesNode.get("x").asDouble();
+            double y = coordinatesNode.get("y").asDouble();
+            System.out.println("Coordinates: "+ x +", " +y );
+            return new Position(x, y);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Function<String, Position> extractFromJSONFunction() {
+        return new Function<String, Position>() {
+            @Override
+            public Position apply(String s) {
+                return extractFromJSON(s);
+            }
+        };
     }
 }
