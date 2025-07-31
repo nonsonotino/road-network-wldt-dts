125dce4925074261a09d1e3fda42509532a85c6a
Matteo Sartini <matteo.sartini26@gmail.com>
2025-07-31 09:55:09 +0200

    Added cars and plate reader dts

--- src/main/java/io/github/wodt/Launcher.java
+++ src/main/java/io/github/wodt/Launcher.java
@@ -6,6 +6,7 @@ import io.github.webbasedwodt.adapter.WoDTDigitalAdapter;
 import io.github.webbasedwodt.adapter.WoDTDigitalAdapterConfiguration;
 import io.github.webbasedwodt.model.dtd.DTVersion;
 import io.github.wodt.digitaladapter.ConsoleDigitalAdapter;
+import io.github.wodt.model.PlateReaderRecord;
 import io.github.wodt.model.Position;
 import io.github.wodt.semantics.CarSemantics;
 import io.github.wodt.shadowing.MirrorShadowingFunction;
@@ -35,9 +36,11 @@ public final class Launcher {
 
     static {
         //Check existence of environment variables.
+        /*
         Objects.requireNonNull(System.getenv(CAR1_EXPOSED_PORT), "Please provide the Car1 DT exposed port");
         Objects.requireNonNull(System.getenv(CAR2_EXPOSED_PORT), "Please provide the Car2 DT exposed port");
         Objects.requireNonNull(System.getenv(CAR3_EXPOSED_PORT), "Please provide the Car3 DT exposed port");
+        */
         //Objects.requireNonNull(System.getenv(MISSION_PLATFORM_URL_VARIABLE), "Please provide the platform url");
     }
 
@@ -52,14 +55,17 @@ public final class Launcher {
             System.out.println("Project launched.");
 
             //Port numbers.
+            /*
             final int car1PortNumber = Integer.parseInt(System.getenv(CAR1_EXPOSED_PORT));
             final int car2PortNumber = Integer.parseInt(System.getenv(CAR2_EXPOSED_PORT));
             final int car3PortNumber = Integer.parseInt(System.getenv(CAR3_EXPOSED_PORT));
+             */
 
             //DT IDs.
             final String car1DTId = "car1-dt";
             final String car2DTId = "car2-dt";
             final String car3DTId = "car3-dt";
+            final String prDTId = "pr-dt";
 
             //DTs initialization.
             final DigitalTwin car1DT = new DigitalTwin(car1DTId,
@@ -68,6 +74,8 @@ public final class Launcher {
                     new MirrorShadowingFunction("car2-shadowing-function"));
             final DigitalTwin car3DT = new DigitalTwin(car3DTId,
                     new MirrorShadowingFunction("car3-shadowing-function"));
+            final DigitalTwin prDT = new DigitalTwin(prDTId,
+                    new MirrorShadowingFunction("pr-shadowing-function"));
 
             //DT digital adapter.
             /* TODO: reinitialize to WoDT DA.
@@ -84,36 +92,44 @@ public final class Launcher {
             );
             */
             ConsoleDigitalAdapter car1DigitalAdapter = new ConsoleDigitalAdapter("car1-da");
-            ConsoleDigitalAdapter car2DigitalAdapter = new ConsoleDigitalAdapter("car1-da");
-            ConsoleDigitalAdapter car3DigitalAdapter = new ConsoleDigitalAdapter("car1-da");
+            ConsoleDigitalAdapter car2DigitalAdapter = new ConsoleDigitalAdapter("car2-da");
+            ConsoleDigitalAdapter car3DigitalAdapter = new ConsoleDigitalAdapter("car3-da");
+            ConsoleDigitalAdapter prDigitalAdapter = new ConsoleDigitalAdapter("pr-da");
 
             //DT mqtt physical adapter.
             //Car 1.
-            MqttPhysicalAdapterConfiguration physicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
+            MqttPhysicalAdapterConfiguration car1PhysicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
                     .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car1", Position.extractFromJSONFunction())
                     .addPhysicalAssetEventAndTopic("movement", " ", "car1/events/movement", Position.extractFromJSONFunction())
                     .build();
-            MqttPhysicalAdapter car1PhysicalAdapter = new MqttPhysicalAdapter("car1-mqtt-pa", physicalAdapterConfiguration);
+            MqttPhysicalAdapter car1PhysicalAdapter = new MqttPhysicalAdapter("car1-mqtt-pa", car1PhysicalAdapterConfiguration);
             //Car 2.
-            physicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
+            MqttPhysicalAdapterConfiguration car2PhysicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
                     .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car2", Position.extractFromJSONFunction())
                     .addPhysicalAssetEventAndTopic("movement", " ", "car2/events/movement", Position.extractFromJSONFunction())
                     .build();
-            MqttPhysicalAdapter car2PhysicalAdapter = new MqttPhysicalAdapter("car2-mqtt-pa", physicalAdapterConfiguration);
+            MqttPhysicalAdapter car2PhysicalAdapter = new MqttPhysicalAdapter("car2-mqtt-pa", car2PhysicalAdapterConfiguration);
             //Car3.
-            physicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
+            MqttPhysicalAdapterConfiguration car3PhysicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
                     .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car3", Position.extractFromJSONFunction())
                     .addPhysicalAssetEventAndTopic("movement", " ", "car3/events/movement", Position.extractFromJSONFunction())
                     .build();
-            MqttPhysicalAdapter car3PhysicalAdapter = new MqttPhysicalAdapter("car3-mqtt-pa", physicalAdapterConfiguration);
+            MqttPhysicalAdapter car3PhysicalAdapter = new MqttPhysicalAdapter("car3-mqtt-pa", car3PhysicalAdapterConfiguration);
+            //Plate reader.
+            MqttPhysicalAdapterConfiguration ptPhysicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
+                    .addPhysicalAssetEventAndTopic("car-detected", " ", "plateReader1/events/car_detected", PlateReaderRecord.extractFromJSONFunction())
+                    .build();
+            MqttPhysicalAdapter prPhysicalAdapter = new MqttPhysicalAdapter("pr-mqtt-pa", ptPhysicalAdapterConfiguration);
 
             //DT adapters configuration.
             car1DT.addDigitalAdapter(car1DigitalAdapter);
             car2DT.addDigitalAdapter(car2DigitalAdapter);
             car3DT.addDigitalAdapter(car3DigitalAdapter);
+            prDT.addDigitalAdapter(prDigitalAdapter);
             car1DT.addPhysicalAdapter(car1PhysicalAdapter);
             car2DT.addPhysicalAdapter(car2PhysicalAdapter);
             car3DT.addPhysicalAdapter(car3PhysicalAdapter);
+            prDT.addPhysicalAdapter(prPhysicalAdapter);
 
             //DT engine creation.
             DigitalTwinEngine digitalTwinEngine = new DigitalTwinEngine();
@@ -122,9 +138,13 @@ public final class Launcher {
             digitalTwinEngine.addDigitalTwin(car1DT);
             digitalTwinEngine.addDigitalTwin(car2DT);
             digitalTwinEngine.addDigitalTwin(car3DT);
+            digitalTwinEngine.addDigitalTwin(prDT);
 
             //Start Engine.
-            digitalTwinEngine.startAll();
+            digitalTwinEngine.startDigitalTwin(car1DTId);
+            digitalTwinEngine.startDigitalTwin(car2DTId);
+            digitalTwinEngine.startDigitalTwin(car3DTId);
+            digitalTwinEngine.startDigitalTwin(prDTId);
 
         } catch (ModelException | WldtDigitalTwinStateException | WldtWorkerException | WldtRuntimeException |
                  EventBusException | WldtConfigurationException | WldtEngineException | MqttException | MqttPhysicalAdapterConfigurationException e) {
