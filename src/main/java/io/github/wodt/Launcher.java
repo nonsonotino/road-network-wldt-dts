6e8a8011985292ae389e0067b79eb4ddc7edf91e
Matteo Sartini <matteo.sartini26@gmail.com>
2025-09-10 10:44:56 +0200

    Updated application launcher

--- src/main/java/io/github/wodt/Launcher.java
+++ src/main/java/io/github/wodt/Launcher.java
@@ -1,29 +1,25 @@
 package io.github.wodt;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.webbasedwodt.adapter.WoDTDigitalAdapter;
 import io.github.webbasedwodt.adapter.WoDTDigitalAdapterConfiguration;
 import io.github.webbasedwodt.model.dtd.DTVersion;
-import io.github.wodt.digitaladapter.ConsoleDigitalAdapter;
 import io.github.wodt.model.PlateReaderRecord;
 import io.github.wodt.model.Position;
 import io.github.wodt.semantics.CarSemantics;
-import io.github.wodt.shadowing.MirrorShadowingFunction;
+import io.github.wodt.semantics.PlateReaderSemantics;
+import io.github.wodt.shadowing.VehicleShadowingFunction;
+import io.github.wodt.shadowing.PRShadowingFunction;
 import it.wldt.adapter.mqtt.physical.MqttPhysicalAdapter;
 import it.wldt.adapter.mqtt.physical.MqttPhysicalAdapterConfiguration;
 import it.wldt.adapter.mqtt.physical.exception.MqttPhysicalAdapterConfigurationException;
-import it.wldt.adapter.mqtt.physical.topic.incoming.DigitalTwinIncomingTopic;
+import it.wldt.adapter.physical.PhysicalAssetRelationship;
 import it.wldt.core.engine.DigitalTwin;
 import it.wldt.core.engine.DigitalTwinEngine;
 import it.wldt.exception.*;
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Objects;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.logging.Logger;
 
 public final class Launcher {
@@ -69,28 +65,15 @@ public final class Launcher {
 
             //DTs initialization.
             final DigitalTwin car1DT = new DigitalTwin(car1DTId,
-                    new MirrorShadowingFunction("car1-shadowing-function"));
+                    new VehicleShadowingFunction("car1-shadowing-function"));
             final DigitalTwin car2DT = new DigitalTwin(car2DTId,
-                    new MirrorShadowingFunction("car2-shadowing-function"));
+                    new VehicleShadowingFunction("car2-shadowing-function"));
             final DigitalTwin car3DT = new DigitalTwin(car3DTId,
-                    new MirrorShadowingFunction("car3-shadowing-function"));
+                    new VehicleShadowingFunction("car3-shadowing-function"));
             final DigitalTwin prDT = new DigitalTwin(prDTId,
-                    new MirrorShadowingFunction("pr-shadowing-function"));
+                    new PRShadowingFunction("pr-shadowing-function"));
 
             //DT digital adapter.
-            /* TODO: reinitialize to WoDT DA.
-            WoDTDigitalAdapter car1DigitalAdapter = new WoDTDigitalAdapter(
-                    "car1-DA",
-                    new WoDTDigitalAdapterConfiguration(
-                            new URI("http://localhost:" + car1PortNumber),
-                            new DTVersion(1,0,0),
-                            new CarSemantics(),
-                            car1PortNumber,
-                            "car1PA",
-                            Set.of()
-                    )
-            );
-            */
             WoDTDigitalAdapter car1DigitalAdapter = new WoDTDigitalAdapter(
                     "car1-da",
                     new WoDTDigitalAdapterConfiguration(
@@ -121,38 +104,51 @@ public final class Launcher {
                             "ABC125",
                             Set.of(URI.create("http://localhost:57381"))
                     ));
-            ConsoleDigitalAdapter prDigitalAdapter = new ConsoleDigitalAdapter("pr-da");
+            WoDTDigitalAdapter prDigitalAdapter = new WoDTDigitalAdapter(
+                    "pr-da",
+                    new WoDTDigitalAdapterConfiguration(
+                            URI.create("http://localhost:57399"),
+                            new DTVersion(1,0,0),
+                            new PlateReaderSemantics(),
+                            57399,
+                            "PR123",
+                            Set.of(URI.create("http://localhost:57381"))
+            ));
 
             //DT mqtt physical adapter.
             //Car 1.
             MqttPhysicalAdapterConfiguration car1PhysicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
-                    .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car1", Position.extractFromJSONFunction())
-                    .addPhysicalAssetEventAndTopic("movement", " ", "car1/events/movement", Position.extractFromJSONFunction())
+                    .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car1/events/movement", Position.extractFromJSONFunction())
+                    //.addPhysicalAssetEventAndTopic("movement", " ", "car1/events/movement", Position.extractFromJSONFunction())
                     .build();
             MqttPhysicalAdapter car1PhysicalAdapter = new MqttPhysicalAdapter("car1-mqtt-pa", car1PhysicalAdapterConfiguration);
             //Car 2.
             MqttPhysicalAdapterConfiguration car2PhysicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
-                    .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car2", Position.extractFromJSONFunction())
-                    .addPhysicalAssetEventAndTopic("movement", " ", "car2/events/movement", Position.extractFromJSONFunction())
+                    .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car2/events/movement", Position.extractFromJSONFunction())
+                    //.addPhysicalAssetEventAndTopic("movement", " ", "car2/events/movement", Position.extractFromJSONFunction())
                     .build();
             MqttPhysicalAdapter car2PhysicalAdapter = new MqttPhysicalAdapter("car2-mqtt-pa", car2PhysicalAdapterConfiguration);
             //Car3.
             MqttPhysicalAdapterConfiguration car3PhysicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
-                    .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car3", Position.extractFromJSONFunction())
-                    .addPhysicalAssetEventAndTopic("movement", " ", "car3/events/movement", Position.extractFromJSONFunction())
+                    .addPhysicalAssetPropertyAndTopic("position", new Position(0, 0), "car3/events/movement", Position.extractFromJSONFunction())
+                    //.addPhysicalAssetEventAndTopic("movement", " ", "car3/events/movement", Position.extractFromJSONFunction())
                     .build();
             MqttPhysicalAdapter car3PhysicalAdapter = new MqttPhysicalAdapter("car3-mqtt-pa", car3PhysicalAdapterConfiguration);
             //Plate reader.
             MqttPhysicalAdapterConfiguration ptPhysicalAdapterConfiguration = MqttPhysicalAdapterConfiguration.builder("localhost", 1883)
-                    .addPhysicalAssetEventAndTopic("car-detected", " ", "plateReader1/events/car_detected", PlateReaderRecord.extractFromJSONFunction())
+                    .addPhysicalAssetEventAndTopic("car-detection", "detection", "plateReader1/events/car_detected", PlateReaderRecord.extractFromJSONFunction())
                     .build();
+            ptPhysicalAdapterConfiguration.getPhysicalAssetDescription().getRelationships().add(new PhysicalAssetRelationship<>("vehicle-detection", "detection"));
             MqttPhysicalAdapter prPhysicalAdapter = new MqttPhysicalAdapter("pr-mqtt-pa", ptPhysicalAdapterConfiguration);
 
             //DT adapters configuration.
+            //Digital
             car1DT.addDigitalAdapter(car1DigitalAdapter);
             car2DT.addDigitalAdapter(car2DigitalAdapter);
             car3DT.addDigitalAdapter(car3DigitalAdapter);
             prDT.addDigitalAdapter(prDigitalAdapter);
+
+            //Physical
             car1DT.addPhysicalAdapter(car1PhysicalAdapter);
             car2DT.addPhysicalAdapter(car2PhysicalAdapter);
             car3DT.addPhysicalAdapter(car3PhysicalAdapter);
