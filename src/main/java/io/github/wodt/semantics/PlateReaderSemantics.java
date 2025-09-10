9a4e2f9d9f66a8417807216378a4a88e592fdabd
Matteo Sartini <matteo.sartini26@gmail.com>
2025-09-10 10:41:17 +0200

    Added semantic definition to detection relationship

--- src/main/java/io/github/wodt/semantics/PlateReaderSemantics.java
+++ src/main/java/io/github/wodt/semantics/PlateReaderSemantics.java
@@ -1,21 +1,26 @@
 package io.github.wodt.semantics;
 
 import io.github.webbasedwodt.model.ontology.DigitalTwinSemantics;
-import io.github.webbasedwodt.model.ontology.rdf.RdfClass;
-import io.github.webbasedwodt.model.ontology.rdf.RdfUnSubjectedTriple;
-import io.github.webbasedwodt.model.ontology.rdf.RdfUriResource;
+import io.github.webbasedwodt.model.ontology.rdf.*;
 import it.wldt.core.state.DigitalTwinStateAction;
 import it.wldt.core.state.DigitalTwinStateProperty;
 import it.wldt.core.state.DigitalTwinStateRelationship;
 import it.wldt.core.state.DigitalTwinStateRelationshipInstance;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class PlateReaderSemantics implements DigitalTwinSemantics {
+
+    private static final Map<String, RdfUriResource> RELATIONSHIP_DOMAIN_TAG = Map.of(
+            "vehicle-detection", new RdfUriResource(URI.create("http://www.example.org/ontology#Car-Detection"))
+    );
+
     @Override
     public List<RdfClass> getDigitalTwinTypes() {
-        return null;
+        return List.of(new RdfClass(URI.create("http://www.example.org/ontology#PlateReader")));
     }
 
     @Override
@@ -25,7 +30,7 @@ public class PlateReaderSemantics implements DigitalTwinSemantics {
 
     @Override
     public Optional<RdfUriResource> getDomainTag(DigitalTwinStateRelationship<?> relationship) {
-        return Optional.empty();
+        return getOptionalFromMap(RELATIONSHIP_DOMAIN_TAG, relationship.getName());
     }
 
     @Override
@@ -40,6 +45,20 @@ public class PlateReaderSemantics implements DigitalTwinSemantics {
 
     @Override
     public Optional<List<RdfUnSubjectedTriple>> mapData(DigitalTwinStateRelationshipInstance<?> relationshipInstance) {
+
+        return getOptionalFromMap(RELATIONSHIP_DOMAIN_TAG, relationshipInstance.getRelationshipName()).map(uri ->
+                List.of(
+                        new RdfUnSubjectedTriple(
+                                new RdfProperty(uri.getUri().get()),
+                                new RdfIndividual(URI.create(relationshipInstance.getTargetId().toString()))
+                        )
+                ));
+    }
+
+    private <T> Optional<T> getOptionalFromMap(final Map<String, T> map, final String key) {
+        if (map.containsKey(key)) {
+            return Optional.of(map.get(key));
+        }
         return Optional.empty();
     }
 }
